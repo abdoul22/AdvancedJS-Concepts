b24df62b38c9875a7a395e9d09351dcfb19f7d1f
Abdou <abdoullaha442@gmail.com>
2023-03-02 15:46:27 +0100

    Error Handeling

--- eventListner.js
+++ eventListner.js
@@ -2,6 +2,12 @@ document.getElementById("btn").addEventListener("click", () => {
   console.log("clicked.");
   let firstNumber = document.getElementById("firstNumber").value;
   let secondNumber = document.getElementById("secondNumber").value;
-  let calc = divid(firstNumber, secondNumber);
-  let result = (document.getElementById("result").innerHTML = calc);
+  
+  try{
+      let calc = divid(firstNumber, secondNumber);
+     document.getElementById("result").innerHTML = calc;
+}catch(error){
+    console.log(error)
+     document.getElementById('result').innerHTML = error
+}
 });
--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
 let divid = (firstNumber, secondNumber) => {
+    if(secondNumber == 0){
+        throw "Essayer de refaire sans diviser par 0";
+    }
   return firstNumber / secondNumber;
 };
